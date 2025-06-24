a04613204af216997871963051e9948cc0982384
nxurhan <nourhan.kheir@gmail.com>
2025-06-24 14:12:59 +1000

    fix testing weather-station.controller

--- src/weather-stations/weather-stations.controller.spec.ts
+++ src/weather-stations/weather-stations.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WeatherStationsController } from './weather-stations.controller';
+import { WeatherStationsService } from './weather-stations.service';
 
 describe('WeatherStationsController', () => {
   let controller: WeatherStationsController;
@@ -7,9 +8,26 @@ describe('WeatherStationsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WeatherStationsController],
+      providers: [
+        WeatherStationsService,
+        {
+          provide: 'WeatherStationRepository',
+          useValue: {},
+        },
+        {
+          provide: 'VariableRepository',
+          useValue: {},
+        },
+        {
+          provide: 'MeasurementRepository',
+          useValue: {},
+        },
+      ],
     }).compile();
 
-    controller = module.get<WeatherStationsController>(WeatherStationsController);
+    controller = module.get<WeatherStationsController>(
+      WeatherStationsController,
+    );
   });
 
   it('should be defined', () => {
